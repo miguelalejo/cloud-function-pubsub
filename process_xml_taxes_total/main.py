32566e5d72949a084be3d1a68316dd6ba945d56a
mapp190413 <mapp190413@sri.ad>
2022-07-29 15:49:10 -0500

    create batch

--- process_xml_taxes_total/main.py
+++ process_xml_taxes_total/main.py
@@ -11,6 +11,13 @@ def createObject(mgConector,groupId,blobExcel,ruc):
         print(e)
         print("Errro insert")
 
+def createBatchProcess(mgConector,groupId,nFiles):
+    try:
+        mgConector.insert_one(bd_name="edocuments",collecion="pending_groups", value={'group_id': groupId, 'nFiles': nFiles,  'status':'PENDING'}) 
+    except DuplicateKeyError as e:
+        print(e)
+        print("Errro insert")
+
 def readfile(filePath):
      in_file = open(filePath, "rb") # opening for [r]eading as [b]inary
      data = in_file.read() # if you only wanted to read 512 bytes, do .read(512)
@@ -32,29 +39,34 @@ def hello_pubsub(event, context):
      mgConectorServ = MongoServiceConector(uri, user, password)
      groupId = int(json_object['idTransaction'])
      nFiles = int(json_object['nFiles'])
-     documents = mgConectorServ.find(bd_name="edocuments",collecion="bills",query= {'group_id': groupId },projection={ "group_id": 1, "_id": 1 ,"file_name":1,"blob_xml":1})
-     listComprobantes = []
-     for doc in documents:
-          if "blob_xml" in doc.keys():
-               print(doc)
-               print("Generar Blob")
-               fileBlob = doc["blob_xml"]
-               print(fileBlob)
-               procesarComprobante = ProcessadorXML(fileBlob) 
-               comprobanteTO = procesarComprobante.procesar()
-               listComprobantes.append(comprobanteTO)
-     print("Tamanio lista",len(listComprobantes))
+     documents = mgConectorServ.find(bd_name="edocuments",collecion="bills",query= {'group_id': groupId },projection={ "group_id": 1, "_id": 1 ,"file_name":1,"blob_xml":1})     
+     print("Tamanio lista",len(documents))
      print("Tamanio lista esperado",nFiles)
-     if len(listComprobantes):
-          print("Generar Reporte")
-          generarReporte = GenerarReporte()
-          reporte,ruc = generarReporte.crearReporteDevIva(listComprobantes)
-          filePath = generarReporte.exportarReporte(reporte,groupId)
-          print("Generarado Reporte")
-          blobExcel = readfile(filePath)
-          createObject(mgConectorServ,groupId,blobExcel,ruc)
-          print("Enviado Reporte")
+     if len(documents) == nFiles:
+          listComprobantes = []
+          for doc in documents:
+               if "blob_xml" in doc.keys():
+                    print(doc)
+                    print("Generar Blob")
+                    fileBlob = doc["blob_xml"]
+                    print(fileBlob)
+                    procesarComprobante = ProcessadorXML(fileBlob) 
+                    comprobanteTO = procesarComprobante.procesar()
+                    listComprobantes.append(comprobanteTO)
+          
+          
+          if len(listComprobantes):
+               print("Generar Reporte")
+               generarReporte = GenerarReporte()
+               reporte,ruc = generarReporte.crearReporteDevIva(listComprobantes)
+               filePath = generarReporte.exportarReporte(reporte,groupId)
+               print("Generarado Reporte")
+               blobExcel = readfile(filePath)
+               createObject(mgConectorServ,groupId,blobExcel,ruc)
+               print("Enviado Reporte")
+          
+          print(pubsub_message)
+     else
+          createBatchProcess(mgConectorServ,groupId,nFiles)
      
-     print(pubsub_message)
-
 
