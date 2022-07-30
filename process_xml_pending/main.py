5ad8aa3bd765dc8a2027ee974fbd96c62795f147
miguelalejo <mikasomk34@hotmail.com>
2022-07-29 19:25:05 -0500

    add chas

--- process_xml_pending/main.py
+++ process_xml_pending/main.py
@@ -6,7 +6,7 @@ from bson import ObjectId
 def updateOne(mgConector,id):
     try:
         newvalues = { "$set": { "status": "DONE" } }
-        mgConector.update_one(bd_name="edocuments",collecion="pending_groups",query={"_id":ObjectId(id)}, value=newvalues) 
+        mgConector.update_one(bd_name="edocuments",collecion="pending_groups",query={"_id":id}, value=newvalues) 
     except DuplicateKeyError as e:
         print(e)
         print("Errro insert")
